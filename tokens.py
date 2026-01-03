fa6bffcd7b2e4082f89845e0f89bccc489dded28
SiokhMatviu <siokhmatviu@gmail.com>
2026-01-03 09:35:01 +0200

    rename reqiu....txt

--- tokens.py
+++ tokens.py
@@ -2,7 +2,6 @@ from authx import AuthX, AuthXConfig
 
 
 config = AuthXConfig()
-config.JWT_SECRET_KEY = "Secret_Key"
 config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
 config.JWT_TOKEN_LOCATION = ["cookies"]
 
